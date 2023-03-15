57e413f51b71754c81f94adddafc870e274fbac7
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-15 11:53:01 +0100

    while loop snack 3

--- snack_3/assets/js/main.js
+++ snack_3/assets/js/main.js
@@ -1,17 +1,46 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-const userNumber = []
+// const userNumber = []
 
-let sum;
+// let sum;
 
-for (let i = 0; i < 10; i++) {
-    // const userNumber = Number(prompt("inserisci un numero"));
-    userNumber.push(Number(prompt("inserisci i numeri")));
-    sum = userNumber[i]++;
+// for (let i = 0; i < 10; i++) {
+//     // const userNumber = Number(prompt("inserisci un numero"));
+//     userNumber.push(Number(prompt("inserisci i numeri")));
+//     sum = userNumber[i]++;
+
+// }
+
+// console.log(userNumber);
+
+// console.log(sum)
+
+// WHILE LOOP Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+// let sum = 0;
+
+// for (let y = 0; y < 3; y++) {
+//     let numbers = Number(prompt('Scrivi un numero'));
+//     console.log(numbers)
+
+//     sum = sum + numbers;
+
+// }
+let sum = 0;
+let k = 0
+
+while (k < 10) {
+
+    let numbers = Number(prompt('Scrivi un numero'));
+    console.log(numbers)
+
+    sum = sum + numbers;
+
+    k++
 
 }
 
-console.log(userNumber);
-
-console.log(sum)
+console.log(sum);
